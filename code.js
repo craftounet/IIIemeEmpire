fecb2516df92a2cdebb38d9e3d89de821fb32611
craftounet <mcraftounet@gmail.com>
2026-04-17 14:24:58 +0100

    Create code.js

---
+++ code.js
@@ -0,0 +1,46 @@
+body {
+  font-family: Arial;
+  background: #1e1e2f;
+  color: white;
+  text-align: center;
+}
+
+.container {
+  width: 320px;
+  margin: auto;
+  background: #2c2c3e;
+  padding: 20px;
+  border-radius: 15px;
+}
+
+h1 {
+  color: gold;
+}
+
+label {
+  display: block;
+  margin-top: 10px;
+}
+
+input, select {
+  width: 100%;
+  padding: 5px;
+  margin-top: 5px;
+}
+
+button {
+  margin-top: 20px;
+  padding: 10px;
+  background: darkred;
+  color: white;
+  border: none;
+  cursor: pointer;
+}
+
+#fiche {
+  margin-top: 20px;
+  background: black;
+  padding: 15px;
+  border-radius: 10px;
+  text-align: left;
+}
